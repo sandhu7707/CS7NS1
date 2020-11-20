5bef705cae58fba379c7dfdb63038bf4279a21b2
Prabhjot <sandhu7707@gmail.com>
2020-11-20 15:20:01 +0000

    *

--- runme.sh
+++ runme.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#pulling from git
+
+git clone https://github.com/sandhu7707/CS7NS1.git
+cd CS7NS1
+
 #virtual environment
 
-python3 -m venv env_singhp1/
-source env_singhp1/bin/activate
+python3 -m venv SINGHP1/
+source SINGHP1/bin/activate
 pip install opencv-python
-pip install tf-lite
+pip install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp38-cp38-linux_armv7l.whl
 
-python3 classify_.py --model-name var_len_classifier.tflite --captcha-dir SINGHP1-project2rpi/ --output stuff.txt --symbols symbols.txt
+#classify
+python classifyLite.py --model-name var_len_classifier.tflite --captcha-dir SINGHP1-project2rpi/ --output newStuff.txt --symbols symbols.txt
